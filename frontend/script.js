ef8fd4e06e96994a351b77f5c047a58a87d2a3bf
Mr. Messias <pedromessiaas@icloud.com>
2025-02-25 18:06:42 -0300

    V 1.7

--- frontend/script.js
+++ frontend/script.js
@@ -9,7 +9,7 @@ fetch('http://localhost:5000/api/news')
                 <h2>${news.title}</h2>
                 <p class="date">${news.date}</p>
                 <p>${news.content}</p>
-                <a href="#" class="read-more">Leia Mais</a>
+                <a href="${news.title.toLowerCase().replace(' ', '-')}.html" class="read-more">+</a>
             `;
             container.appendChild(div);
         });
